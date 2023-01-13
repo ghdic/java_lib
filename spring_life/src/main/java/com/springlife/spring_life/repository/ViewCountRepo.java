287638c358a9141a65ce255d639965d899051bee
marinelife <ghdic77@gmail.com>
2023-01-14 08:42:51 +0900

    spring_life 삭제, .idea폴더 ignore

--- spring_life/src/main/java/com/springlife/spring_life/repository/ViewCountRepo.java
+++
@@ -1,9 +0,0 @@
-package com.springlife.spring_life.repository;
-
-import com.springlife.spring_life.model.ViewCount;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ViewCountRepo extends JpaRepository<ViewCount, Long> {
-}
