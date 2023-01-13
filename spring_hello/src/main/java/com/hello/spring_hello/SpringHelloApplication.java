46b06717c265724c54e538d568cc1694cfa2449f
marinelife <ghdic77@gmail.com>
2023-01-14 08:40:42 +0900

    김영한 스프링 기본 강의

---
+++ spring_hello/src/main/java/com/hello/spring_hello/SpringHelloApplication.java
@@ -0,0 +1,18 @@
+package com.hello.spring_hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Stack;
+import java.util.Vector;
+
+@SpringBootApplication
+public class SpringHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHelloApplication.class, args);
+        Stack<Integer> st;
+        Vector<Integer> v;
+    }
+
+}
